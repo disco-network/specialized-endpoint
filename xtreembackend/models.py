1f5aaae3ffc70c926f634715d0972eb2399b0e63
Paul Reichert <datokrat@users.noreply.github.com>
2018-09-30 01:26:29 +0200

    added parent/child relation and parameterized queries

--- xtreembackend/models.py
+++ xtreembackend/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Node(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(max_length=65535)
+    
+    parents = models.ManyToManyField("self", related_name="children", symmetrical=False)
 
     def __str__(self):
         return self.title
+
--- xtreembackend/views.py
+++ xtreembackend/views.py
@@ -4,18 +4,50 @@ from django.shortcuts import render
 from xtreembackend.models import Node
 
 def getNodes(request):
-    ids = [1]
-    options = {
-        "include_id": True,
-        "include_title": True,
-        "include_content": True,
-    }
+    (ids, options) = parseOptions(request)
+
+    nodes = {}
+    for id in ids:
+        getNodesRec(id, nodes, options["parentlevels"], options["childlevels"])
 
     result = {}
-    for id in ids:
-        node = Node.objects.get(id=id) 
+    for id in nodes:
+        node = nodes[id]["node"]
         result[id] = serializeToJson(node, options)
     return JsonResponse(result)
+
+def parseOptions(request):
+    neededFields = request.GET.getlist("neededFields[]", ["title", "content"])
+    ids = [int(x) for x in request.GET.getlist("ids[]")]
+
+    options = {
+        "include_id": True,
+        "include_title": "title" in neededFields,
+        "include_content": "content" in neededFields,
+        # TODO: error handling
+        "parentlevels": int(request.GET.get("parentlevels", default=0)),
+        "childlevels": int(request.GET.get("childlevels", default=0)),
+    }
+
+    return (ids, options)
+
+def getNodesRec(id, nodes, parentdepth, childdepth):
+    if not id in nodes:
+        nodes[id] = {
+            "node": Node.objects.get(id=id),
+            "childdepth": 0,
+            "parentdepth": 0
+        }
+
+    if nodes[id]["childdepth"] < childdepth:
+        nodes[id]["childdepth"] = childdepth
+        for child in nodes[id]["node"].children.all():
+            getNodesRec(child.id, nodes, 0, childdepth - 1)
+
+    if nodes[id]["parentdepth"] < parentdepth:
+        nodes[id]["parentdepth"] = parentdepth
+        for parent in nodes[id]["node"].parents.all():
+            getNodesRec(parent.id, nodes, parentdepth - 1, 0)
 
 def serializeToJson(node, options):
     fields = {
@@ -24,7 +56,10 @@ def serializeToJson(node, options):
         "content": node.content,
     }
 
-    json = {}
+    json = {
+        "parents": [parent.id for parent in node.parents.all()],
+        "children": [child.id for child in node.children.all()],
+    }
 
     for key in fields:
         option = "include_" + key
